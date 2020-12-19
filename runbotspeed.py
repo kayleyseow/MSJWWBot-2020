035854bf4e1cdc58e79740333c5f5b5bc5f5a52b
Kayley Seow <kayleyseow8@gmail.com>
2020-12-18 23:57:44 -0800

    Commenting for runbotspeed.py

--- runbotspeed.py
+++ runbotspeed.py
@@ -1,3 +1,4 @@
+#runbotwindows.py at a considerably faster speed
 # I AM SPEED
 import instabot
 import time
@@ -5,22 +6,22 @@ import random
 import os
 
 
-def main():
+def main(): #comments found on runbotwindows.py
     bot = instabot.Bot()
     bot.login()
-    pauseseconds = random.randrange(1, 5)
+    pauseseconds = random.randrange(1, 5) #faster range of times between posts
     os.system("copy msjww.jpg msjwwcopy.jpg")
     counter = 1;
 
-    while(True):
+    while(True): #comments found on runbotwindows.py
         bot.upload_photo("msjwwcopy.jpg", caption="#msjww20")
         print("Waiting: ", pauseseconds)
         print("Number of posts: ", counter)
         counter += 1
         time.sleep(pauseseconds)
-        pauseseconds = random.randrange(1, 5)
+        pauseseconds = random.randrange(1, 5) #faster range of times between posts
         os.system("del \\f msjwwcopy.jpg.REMOVE_ME")
         os.system("copy msjww.jpg msjwwcopy.jpg")
 
-if __name__ == "__main__":
-	main() #run main
+if __name__ == "__main__": #comments found on runbotwindows.py
+	main()
